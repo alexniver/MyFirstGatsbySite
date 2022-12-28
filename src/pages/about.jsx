c98bac2d31c195a5273c0ea671a8adfaaf57765a
Alexniver <zhulin10541@gmail.com>
2022-12-28 15:49:55 +0800

    finish tutorial 3

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const About = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>This is About Page</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About me title</title>
+
+export default About
